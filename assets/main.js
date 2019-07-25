d8b7aafe895c0913e5cf6d9b2d176ab1945c2600
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-07-25 14:30:25 -0400

    wrapped the menu overlay function in a IIFE + refactoring css file

--- assets/main.js
+++ assets/main.js
@@ -1,13 +1,17 @@
 ---
 ---
 
-let burgerIcon = document.getElementById("menu-icon");
+// toggles menu overlay
+(function initMenuOverlay() {
+  let burgerIcon = document.getElementById("menu-icon");
 
-burgerIcon.addEventListener("click", function() {
+  burgerIcon.addEventListener("click", function() {
   let overlay = document.getElementsByClassName("overlay")[0];
   let bars = document.getElementsByClassName("menu-bars")[0];
 
   burgerIcon.style.zIndex = "1";
   bars.classList.toggle("change");
   overlay.classList.toggle("active");
-});
+
+  });
+})();
