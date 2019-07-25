2a9d509c7d8fc4d33df2907bc782f623bfbe7cab
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-07-25 02:54:29 -0400

    added a menu overlay + JS/styling to toggle visibility

--- assets/main.js
+++ assets/main.js
@@ -1,6 +1,13 @@
 ---
 ---
 
-function myFunction(x) {
-  x.classList.toggle("change");
-}
+let burgerIcon = document.getElementById("menu-icon");
+
+burgerIcon.addEventListener("click", function() {
+  let overlay = document.getElementsByClassName("overlay")[0];
+  let bars = document.getElementsByClassName("menu-bars")[0];
+
+  burgerIcon.style.zIndex = "1";
+  bars.classList.toggle("change");
+  overlay.classList.toggle("active");
+});
