0ea28e57314b5a91d97dfb99f039481d92fb1e18
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-08-09 14:01:09 -0400

    successfully added + tested aws gateway api for contact form functionality

--- assets/handler.js
+++ assets/handler.js
@@ -1,23 +1,56 @@
-var CONTACT_ADDRESS = "fhernand0705@gmail.com";
-var querystring = require('querystring');
+function submitToAPI(e) {
+       e.preventDefault();
+       var URL = "https://fz75fz7jc1.execute-api.us-east-1.amazonaws.com/contact/contact";
 
-var mailer = require('nodemailer').createTransport({
-  service = "Gmail";
-  auth: {
-    user: process.env.EMAIL_USERNAME,
-    pass: process.env.EMAIL_PASSWORD,
-  }
-});
+            // var Namere = /[A-Za-z]{1}[A-Za-z]/;
+            // if (!Namere.test($("#name-input").val())) {
+            //              alert ("Name can not less than 2 char");
+            //     return;
+            // }
+            // var mobilere = /[0-9]{10}/;
+            // if (!mobilere.test($("#phone-input").val())) {
+            //     alert ("Please enter valid mobile number");
+            //     return;
+            // }
+            // if ($("#email-input").val()=="") {
+            //     alert ("Please enter your email id");
+            //     return;
+            // }
+            //
+            // var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+            // if (!reeamil.test($("#email-input").val())) {
+            //     alert ("Please enter valid email address");
+            //     return;
+            // }
 
-module.exports.contact = (event, context, callback) => {
-  var body = querystring.parse(event.body);
-  mailer.sendMail({
-    from: body.from,
-    to: [CONTACT_ADDRESS],
-    subject: body.subject || '[No subject]',
-    html: body.message || '[No message]',
-  }, function(err, info) {
-    if (err) return callback(err);
-    callback(null, {statusCode: 200, body: "Success!"});
-  });
-};
+       var name = $("#name-input").val();
+       var email = $("#email-input").val();
+       var subject = $("#subject-input").val();
+       var desc = $("#text-input").val();
+       var data = {
+          name : name,
+          email : email,
+          subject : subject,
+          desc : desc
+        };
+
+       $.ajax({
+         type: "POST",
+         url : URL,
+         dataType: "json",
+         crossDomain: "true",
+         contentType: "application/json; charset=utf-8",
+         data: JSON.stringify(data),
+
+
+         success: function () {
+           // clear form and show a success message
+           alert("Successfull");
+           document.getElementById("contact-form").reset();
+       location.reload();
+         },
+         error: function () {
+           // show an error message
+           alert("Unsuccessfull");
+         }});
+     }
