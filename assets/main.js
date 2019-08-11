e6ee9988d1bade29b7a51fc47a92f463bd5c83c4
Freddy Hernandez <freddy.hernandez@fandm.edu>
2019-08-11 19:27:16 -0400

    added bg image + styling

--- assets/main.js
+++ assets/main.js
@@ -1,7 +1,7 @@
 ---
 ---
 
-// toggles menu overlay
+// toggles nav menu overlay
 (function initMenuOverlay() {
   let burgerIcon = document.getElementById("menu-icon");
 
@@ -16,36 +16,39 @@
   });
 })();
 
+// validates contact form user data + invokes aws gateway api
 function submitToAPI(e) {
        e.preventDefault();
-       var URL = "https://fz75fz7jc1.execute-api.us-east-1.amazonaws.com/contact/contact";
+       let URL = "https://fz75fz7jc1.execute-api.us-east-1.amazonaws.com/contact/contact";
 
-            // var Namere = /[A-Za-z]{1}[A-Za-z]/;
-            // if (!Namere.test($("#name-input").val())) {
-            //              alert ("Name can not less than 2 char");
-            //     return;
-            // }
-            // var mobilere = /[0-9]{10}/;
-            // if (!mobilere.test($("#phone-input").val())) {
-            //     alert ("Please enter valid mobile number");
-            //     return;
-            // }
-            // if ($("#email-input").val()=="") {
-            //     alert ("Please enter your email id");
-            //     return;
-            // }
-            //
-            // var reeamil = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
-            // if (!reeamil.test($("#email-input").val())) {
-            //     alert ("Please enter valid email address");
-            //     return;
-            // }
+            let checkFname = /[A-Za-z]{1}[A-Za-z]/;
+            if (!checkFname.test($("#fname-input").val())) {
+                alert ("Name cannot be less than two characters");
+                return;
+            }
 
-       var name = $("#name-input").val();
-       var email = $("#email-input").val();
-       var subject = $("#subject-input").val();
-       var desc = $("#text-input").val();
-       var data = {
+            let checkLname = /[A-Za-z]{1}[A-Za-z]/;
+            if (!checkLname.test($("#lname-input").val())) {
+                alert ("Name cannot be less than two characters");
+                return;
+            }
+            if ($("#email-input").val()=== "") {
+                alert ("Please enter your email address");
+                return;
+            }
+
+            let checkEmail = /^([\w-\.]+@([\w-]+\.)+[\w-]{2,6})?$/;
+            if (!checkEmail.test($("#email-input").val())) {
+                alert ("Please enter valid email address");
+                return;
+            }
+
+       let fname = $("#fname-input").val();
+       let lname = $("#lname-input").val();
+       let email = $("#email-input").val();
+       let subject = $("#subject-input").val();
+       let desc = $("#message-input").val();
+       let data = {
           name : name,
           email : email,
           subject : subject,
@@ -60,12 +63,11 @@ function submitToAPI(e) {
          contentType: "application/json; charset=utf-8",
          data: JSON.stringify(data),
 
-
          success: function () {
            // clear form and show a success message
            alert("Successfull");
            document.getElementById("contact-form").reset();
-       location.reload();
+           location.reload();
          },
          error: function () {
            // show an error message
